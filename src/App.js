77ec4007fc3ce3cc32245ed13b6dfb0fd6d298e8
JVCarv41 <jvc.04.off@gmail.com>
2025-03-21 16:06:26 -0300

    Initial commit
    initial commit with all the files for the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [birthdate, setBirthdate] = useState("");
+  const [age, setAge] = useState({ years: 0, months: 0, days: 0 });
+
+  const calculateAge = () => {
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+
+    if (isNaN(birthDate.getTime())) {
+      alert("Please enter a valid date.");
+      return;
+    }
+
+    let years = today.getFullYear() - birthDate.getFullYear();
+    let months = today.getMonth() - birthDate.getMonth();
+    let days = today.getDate() - birthDate.getDate();
+
+    if (days < 0) {
+      months--;
+      days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    setAge({ years, months, days });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculador de Idade</h1>
+      <div className="calculator">
+        <label htmlFor="birthdate">Insira sua data de Nascimento:</label>
+        <input
+          type="date"
+          id="birthdate"
+          value={birthdate}
+          onChange={(e) => setBirthdate(e.target.value)}
+        />
+        <button onClick={calculateAge}>Calcular Idade</button>
+      </div>
+      {
+        <div className="result">
+          <p>
+            Sua idade é: <strong>{age.years}</strong> anos,{" "}
+            <strong>{age.months}</strong> meses e <strong>{age.days}</strong>{" "}
+            dias.
+          </p>
+        </div>
+      }
     </div>
   );
 }
